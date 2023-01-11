b9785acd8c7ae08cd062896c38ae2c5f2ccd7363
Girish Kumar <gjyanyani@gmail.com>
2023-01-11 09:55:26 +0530

    not found page added for wrong url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path='/cart' element={<Cart/>} />
+        <Route path='/not-found' element={<NotFound/>} />
         <Route path='/' element={<Home/>} />
+        <Route path='*' element={<Navigate replace to='/not-found' />} />
       </Routes>
       </Router>
     </div>
